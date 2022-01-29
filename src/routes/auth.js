6dcb040c8b802bbf64f594cd67d18c77c5f39014
Eniz <freessb1@hotmail.com>
2022-01-29 20:38:25 +0300

    create auth files

---
+++ src/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const authControllers = require('../controllers/auth');
+
+const router = express.Router();
+
+// router.get('/signin',authControllers.login)
+// router.get('/signout',authControllers.login)
