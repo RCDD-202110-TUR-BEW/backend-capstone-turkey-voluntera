b6484fa6a10d279f7f0f21597a5a19a6a40158df
Eniz <freessb1@hotmail.com>
2022-02-08 23:16:33 +0300

    add strategy callback

--- src/utils/google.js
+++ src/utils/google.js
@@ -1,14 +1,40 @@
-const passport = require('passport');
 const GoogleStrategy = require('passport-google-oidc');
+const { User, Volunteer } = require('../models/user');
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GAPP_CLIENT,
-      clientSecret: process.env.GAPP_SECRET,
-      callBackUrl: 'http://localhost:3000/auth/google/callback',
-      scope: ['profile', 'email', 'openid'],
-    },
-    (accessToken, refreshToken, profile, cb) => {}
-  )
+const googleStr = new GoogleStrategy(
+  {
+    clientID: process.env.GAPP_CLIENT,
+    clientSecret: process.env.GAPP_SECRET,
+    callbackURL: 'http://localhost:8000/auth/google/callback',
+    scope: ['profile', 'email', 'openid'],
+  },
+  async (_, profile, cb) => {
+    /* 
+    Check if a user with googleId or email exists. 
+    Create user if there is no user associated with credentials 
+    */
+    User.findOne(
+      {
+        $or: [{ googleId: profile.id }, { email: profile.emails[0].value }],
+      },
+      (err, user) => {
+        if (err) return cb(err);
+
+        if (!user) {
+          const newUser = {
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            firstName: profile.name.givenName,
+            lastname: profile.name.familyName,
+          };
+          const createdUser = Volunteer.create(newUser);
+          return cb(null, createdUser);
+        }
+
+        return cb(null, user);
+      }
+    );
+  }
 );
+
+module.exports = googleStr;
