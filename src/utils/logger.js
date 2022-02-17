117b843573c35a6767ca4c22935e2419f895a28e
ibraheemhajjar <79800389+ibraheemhajjar@users.noreply.github.com>
2022-02-18 00:55:34 +0300

    update logger.js
    set logging destination

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,14 +1,15 @@
-const pino = require('pino');
+const logger = require('pino');
 
-const logger = pino({
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
-      ignore: 'pid,hostname',
+module.exports = logger(
+  {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'SYS:dd-mm-yyyy HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
     },
   },
-});
-
-module.exports = logger;
+  logger.destination('../voluntera.log')
+);
