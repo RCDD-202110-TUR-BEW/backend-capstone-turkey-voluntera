03d90372dcbb5e60cfecf8a749f28ca5e1e2a57e
Eniz <freessb1@hotmail.com>
2022-02-23 23:36:55 +0300

    add logger

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,12 @@
+const logger = require('../utils/logger');
+
 const handleErrors = (err, req, res, next) => {
-  //  Add logger when it's merged
-  res.status(500).json({ message: 'Server ran into an error', err });
+  logger.error(err);
+
+  if (res.headersSent) {
+    return next(err);
+  }
+  return res.status(500).json({ message: 'Server ran into an error', err });
 };
 
 module.exports = handleErrors;
